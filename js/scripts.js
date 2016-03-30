d10f972c8e4d1be1c70adaa0829b34cbb6e86762
Blake Bless and Nick Arrasate <nick@>
2016-03-30 11:44:46 -0700

    now works for more than one word.

--- js/scripts.js
+++ js/scripts.js
@@ -7,31 +7,33 @@ $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
 
-    var wordArray = $("input#year").val().toLowerCase().split("");
-    var sliceArray = wordArray.slice(0,1);
-    var sliceArray2 = wordArray.slice(1,2);
+    var wordArray = $("input#year").val().toLowerCase().split(" ");
+
+    wordArray.forEach(function(word){
+
+    var word = word.split("")
+    console.log(word);
+    var sliceArray = word.slice(0,1);
+    var sliceArray2 = word.slice(1,2);
 
 
     if ((sliceArray == "a") || (sliceArray == "e") || (sliceArray == "i") || (sliceArray == "o") || (sliceArray == "u")){
-      $('.display').text(wordArray.join('') + "ay");
+      $('.display').append(word.join('') + "ay ");
     }
      else if ((sliceArray2 != "a") && (sliceArray2 != "e") && (sliceArray2 != "i") && (sliceArray2 != "o") && (sliceArray2 != "u")){
-      var first = wordArray.shift();
-      var second = wordArray.shift();
-      var newone = wordArray.push(first,second);
-      $('.display').text(wordArray.join('') + "ay");
+      var first = word.shift();
+      var second = word.shift();
+      var newone = word.push(first,second);
+      $('.display').append(word.join('') + "ay ");
     }
     else {
-      var first = wordArray.shift();
-      var newone = wordArray.push(first);
-      $('.display').text(wordArray.join('') + "ay");
+      var first = word.shift();
+      var newone = word.push(first);
+      $('.display').append(word.join('') + "ay ");
     }
-
-
-    console.log(wordArray);
+    console.log(word);
     console.log(sliceArray);
-    // $('.display').text(array);
-
+  });
 
 
   });
