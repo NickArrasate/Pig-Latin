b905103f41cb6172ee87100639b1912b9349f404
Blake Bless and Nick Arrasate <nick@>
2016-03-30 10:52:43 -0700

    now takes into account if first and second letter are vowels and constants

--- js/scripts.js
+++ js/scripts.js
@@ -9,10 +9,19 @@ $(document).ready(function() {
 
     var wordArray = $("input#year").val().toLowerCase().split("");
     var sliceArray = wordArray.slice(0,1);
+    var sliceArray2 = wordArray.slice(1,2);
+
 
     if ((sliceArray == "a") || (sliceArray == "e") || (sliceArray == "i") || (sliceArray == "o") || (sliceArray == "u")){
       $('.display').text(wordArray.join('') + "ay");
-    } else {
+    }
+     else if ((sliceArray2 != "a") && (sliceArray2 != "e") && (sliceArray2 != "i") && (sliceArray2 != "o") && (sliceArray2 != "u")){
+      var first = wordArray.shift();
+      var second = wordArray.shift();
+      var newone = wordArray.push(first,second);
+      $('.display').text(wordArray.join('') + "ay");
+    }
+    else {
       var first = wordArray.shift();
       var newone = wordArray.push(first);
       $('.display').text(wordArray.join('') + "ay");
